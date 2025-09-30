5753d6716eccd3fc0ca5396523f73a56ff892683
Brighton Brown <brizziehh@gmail.com>
2025-09-30 13:12:45 +0300

    Update .gitignore to exclude all environment variable files and fix build script path in package.json; enhance URL routing in portfolio app

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import *
 
+app_name = 'projects'
+
 urlpatterns = [
-    path('', list, name="projects"),
-    path('create/', create, name="create_project"),
+    path('', list, name="list"),
+    path('create/', create, name="create"),
+    path('<int:project_id>/edit/', update, name="edit"),
+    path('<int:project_id>/delete/', delete, name="delete"),
 ]
